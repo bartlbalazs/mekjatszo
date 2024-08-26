04c1487b2502df085c464916a4e9cea8f3bb452c
Balázs Bartl <balazs.bartl@aliz.ai>
2024-08-27 00:31:24 +0200

    Player improvements

--- mek-player/nuxt.config.ts
+++ mek-player/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolve from 'unplugin-icons/resolver'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: [
@@ -8,5 +13,17 @@ export default defineNuxtConfig({
   ],
   css: ['bootstrap/dist/css/bootstrap.min.css'],
   ssr: false,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [IconsResolve()],
+        dts: true,
+      }),
+      Icons({
+        compiler: 'vue3',
+        autoInstall: true,
+      }),
+    ],
+  }
 })
--- mek-player/pages/books/[id].vue
+++ mek-player/pages/books/[id].vue
@@ -1,10 +1,61 @@
 <template>
+  <div id="app">
+    <main>
+      <section class="player">
+        <div>
+          <h1>{{ book.author }}</h1>
+          <h2>{{ book.title }}</h2>
+        </div>
+        <div class="cover-wrapper">
+          <img :src="cover" />
+        </div>
+        <div class="song-details">
+          <h2 class="song-title">
+            {{ current.title }}
+          </h2>
+          <KProgress :show-text="false" class="progress-bar-wrapper" v-bind:percent="current.percent"
+            :color="['#df83f1', '#82279f', '#53cfe0']" />
+          <div class="timer">
+            <p class="start">{{ currentlyTimer }}</p>
+            <p class="end">
+              {{ current.totalTimer }}
+            </p>
+          </div>
+        </div>
+        <div class="controls">
+          <button class="prev" @click="prev" v-if="chapters.length > 1">
+            <IFaStepBackward />
+          </button>
+          <button class="play" v-if="!isPlaying" @click="play">
+            <IFaPlay />
+          </button>
+          <button class="pause" v-else @click="pause">
+            <IFaPause />
+          </button>
+          <button class="next" @click="next" v-if="chapters.length > 1">
+            <IFaStepForward />
+          </button>
+        </div>
+      </section>
+    </main>
+    <section class="playlist">
+      <ul>
+        <li v-for="chapter in chapters" :key="chapter.src" class="song">
+          <div class="details" @click="play(chapter)">
+            <h2 class="song-title">
+              {{ chapter.title }}
+            </h2>
+            <KProgress v-if="chapter.isPlaying" :color="['#df83f1', '#82279f', '#53cfe0']" :show-text="false"
+              class="progress-bar-wrapper" v-bind:percent="chapter.percent" />
+          </div>
+        </li>
+      </ul>
+    </section>
+  </div>
   <div>
-    <li v-for="file in book.audio_files">
-      {{ file.title }} -{{ file.url }}
-    </li>
-
-    </div>
+    <p>{{ book.lead }}</p>
+    <p>{{ book.description }}</p>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -23,12 +74,23 @@ interface Book {
   }]
 }
 
-import { ref } from 'vue'
-import axios from 'axios'
+interface Chapter {
+  title: string
+  src: string
+  isPlaying?: boolean
+  percent?: number
+  currentlyTimer?: string
+  totalTimer?: string
+  seconds?: number
+}
 
+import axios from 'axios';
+import { ref } from 'vue';
+import KProgress from "k-progress-v3";
 
 const route = useRoute()
 const book = ref({} as Book)
+const cover = ref('')
 
 async function fetchData() {
   try {
@@ -36,18 +98,117 @@ async function fetchData() {
     const bookRespone = await axios.get<Book>('/static/books/' + route.params.id + '.json')
     if (bookRespone.data) {
       book.value = bookRespone.data;
-      const songs = book.value.audio_files.map(file => ({
-        name: file.title,
-        url: "https://mek.oszk.hu/08800/08820/mp3/" + file.url
+      cover.value = "https://mek.oszk.hu" + book.value.cover
+      chapters.value = book.value.audio_files.map(file => ({
+        title: file.title,
+        src: "https://mek.oszk.hu/" + book.value.id.replace('_', '/') + "/mp3/" + file.url,
       }));
-
+      console.log(chapters.value[0])
+      current.value = chapters.value[0];
     }
   } catch (error) {
     console.log(error);
   }
 };
 
+function formatTimer(seconds: any) {
+  let minutes = parseInt((seconds / 60).toString());
+  seconds = parseInt((seconds % 60).toString());
+
+  let output = minutes >= 10 ? `${minutes}` : `0${minutes}`;
+  output += seconds >= 10 ? `:${seconds}` : `:0${seconds}`;
+
+  return output;
+};
+
+const current = ref({
+  isPlaying: false,
+  percent: 0,
+  currentlyTimer: "00:00",
+  totalTimer: "00:00",
+  seconds: 0,
+  title: "",
+} as Chapter);
+const index = ref(0);
+const isPlaying = ref(false);
+const currentlyTimer = ref("00:00");
+const chapters = ref([{}] as Chapter[]);
+const player = new Audio();
+player.onloadedmetadata = function () {
+  current.value.seconds = player.duration;
+  current.value.totalTimer = formatTimer(player.duration);
+};
+
+function listenersWhenPlay() {
+  player.addEventListener("timeupdate", () => {
+    var playerTimer = player.currentTime;
+
+    currentlyTimer.value = formatTimer(playerTimer);
+    if (current.value.seconds) {
+      current.value.percent = (playerTimer * 100) / current.value.seconds;
+    }
+    current.value.isPlaying = true;
+  });
+  player.addEventListener(
+    "ended",
+    function () {
+      next();
+    }
+  );
+}
+
+function setCurrentChapter() {
+  current.value = chapters.value[index.value];
+  play(current.value);
+};
+
+function play(chapter: Chapter) {
+  if (typeof chapter.src !== "undefined") {
+    current.value.isPlaying = false;
+    index.value = chapters.value.indexOf(current.value);
+    current.value = chapter;
+  } else {
+    player.src = chapters.value[0].src;
+    index.value = 0;
+    current.value = chapters.value[0];
+  }
+  player.src = current.value.src;
+
+  player.play();
+  isPlaying.value = true;
+
+  listenersWhenPlay();
+};
+
+function pause() {
+  player.pause();
+  isPlaying.value = false;
+};
+
+function next() {
+  current.value.isPlaying = false;
+  index.value = chapters.value.indexOf(current.value);
+  index.value++;
+  if (index.value > chapters.value.length - 1) {
+    index.value = 0;
+  }
+  setCurrentChapter();
+};
+
+function prev() {
+  current.value.isPlaying = false;
+  index.value = chapters.value.indexOf(current.value);
+  index.value--;
+  if (index.value < 0) {
+    index.value = chapters.value.length - 1;
+  }
+  setCurrentChapter();
+};
+
 onMounted(() => {
-  fetchData()
+  fetchData();
+  let jsmediatagsScript = document.createElement('script')
+  jsmediatagsScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js')
+  document.head.appendChild(jsmediatagsScript)
 })
 </script>
