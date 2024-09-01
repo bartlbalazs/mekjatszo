ad7216d637c0782fb40d891c0a9751e1de78e281
Balázs Bartl <balazs.bartl@aliz.ai>
2024-09-01 13:47:51 +0200

    Cleaned book and chapter titles

---
+++ docs/_nuxt/Crp9JPLY.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./BlsCus44.js";import"./BnAavg4o.js";export{m as default};
---
+++ docs/_nuxt/DOg9QhGp.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./CNhBwGSj.js";import"./BnAavg4o.js";import"./C-v3KzvZ.js";import"./Dnd51l0P.js";import"./vUuWs-q-.js";export{o as default};
